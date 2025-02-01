cdd487be0225a7532c14c7585f95dde7b4b12f23
Aman Jaiswal <95357926+Amanjazz100@users.noreply.github.com>
2025-02-01 13:00:59 +0000

    dev done in variable

---
+++ 01_BASICS/01_variable.js
@@ -0,0 +1,12 @@
+const accountId = 1234567890;
+let accountEmail = "AmanJaizz100@gmail.com";
+var accountPassword = "12345";
+
+accountCity = "JaiPur";
+
+
+
+
+// accountId = 98765425;
+
+console.log(accountId);
